456227337c33564b84781928ef453d19a4192668
mahadi025 <mahadikarimmunif@gmail.com>
2024-02-02 20:05:28 +0600

    moved the firebase config to .env file

--- firebase.js
+++ firebase.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection } from "firebase/firestore"
+import { getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyApwVEiTaGfTCtoumqCXpvqU56numflPvM",
-    authDomain: "note-app-df99b.firebaseapp.com",
-    projectId: "note-app-df99b",
-    storageBucket: "note-app-df99b.appspot.com",
-    messagingSenderId: "979777551833",
-    appId: "1:979777551833:web:d7d0356e6f0700ad20adc8"
+    apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
-export const notesCollection = collection(db, "notes")
+export const db = getFirestore(app);
+export const notesCollection = collection(db, "notes");
