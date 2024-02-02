ce977f1c0d04defcc73d02fc6e71781e5d171a15
mahadi025 <mahadikarimmunif@gmail.com>
2024-02-02 20:20:05 +0600

    updated environment variable

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
+    apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
+    authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
